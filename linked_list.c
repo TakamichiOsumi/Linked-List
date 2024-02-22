d02b19082cd9a98b7acb4aef7a6537bedbb0ff74
Takamichi Osumi <takamichiosumi01@gmail.com>
2024-02-22 14:01:26 +0000

    Include stdint.h for ubuntu environment.

--- linked_list.c
+++ linked_list.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
--- test_linked_list.c
+++ test_linked_list.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include "linked_list.h"
 
